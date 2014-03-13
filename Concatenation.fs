1169f250c0b17c9978f188c5e1ae18a70c58dbae
Denis Chusovitin <Chusovitin.Den@gmail.com>
2014-03-13 23:13:43 +0400

    First commit

---
+++ Concatenation.fs
@@ -0,0 +1,15 @@
+﻿//Chusovitin Denis Copyrights (c) 2013
+//Concatenate lists
+
+let list1 = [3;6;9]
+let list2 = [2;0]
+
+let rec conc l1 l2 =
+    match l1 with
+        | []-> l2
+        | hd::tl-> hd::conc tl l2
+
+let newlist = conc list1 list2
+
+
+
