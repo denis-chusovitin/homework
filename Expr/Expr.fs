58b80ff86e7d71d766ae1cf10cec13356e814c54
Denis Chusovitin <Chusovitin.Den@gmail.com>
2014-05-29 19:53:51 +0400

    Fix expr

---
+++ Expr/Expr.fs
@@ -0,0 +1,38 @@
+﻿//Chusovitin Denis Copyrights (c) 2014
+//Simplify of expressions
+
+module Expr
+
+type Expr = 
+    | Const of int 
+    | Var of string 
+    | Add of Expr * Expr
+    | Sub of Expr * Expr
+    | Mul of Expr * Expr
+    | Div of Expr * Expr
+
+let rec simpl expr = 
+    let simpl' expr =
+        match expr with
+        | Add (Const x, Const y) -> Const (x + y)
+        | Sub (Const x, Const y) -> Const (x - y)
+        | Mul (Const x, Const y) -> Const (x * y)
+        | Div (Const x, Const y) -> Const (x / y)
+        | Add (exp, Const 0) | Add (Const 0, exp) -> simpl exp
+        | Sub (Var x, Var y) when x = y -> Const 0
+        | Mul (Const 0, _) | Mul (_, Const 0) -> Const 0                  
+        | Mul (exp, Const 1) | Mul (Const 1, exp) -> simpl exp
+        | Div (exp, Const 1) -> simpl exp
+        | _ -> expr
+
+    match expr with
+    | Add (a, b) -> simpl' (Add (simpl a, simpl b))
+    | Sub (a, b) -> simpl' (Sub (simpl a, simpl b))
+    | Mul (a, b) -> simpl' (Mul (simpl a, simpl b))
+    | Div (a, b) -> simpl' (Div (simpl a, simpl b))
+    | _ -> expr
+
+
+
+printfn "%A" (simpl (Add(Div(Const 2, Mul(Const 1, Const 1)), Const 0))) 
+
---
+++ Expr/Test.fs
@@ -0,0 +1,34 @@
+﻿module Test
+
+open Expr
+open NUnit.Framework
+
+[<TestFixture>]
+type Tests() =
+    [<Test>]
+    member x.Test1() = 
+        Assert.AreEqual(simpl (Sub(Var "x", Var "y")), (Sub(Var "x", Var "y")), "error")
+    [<Test>]
+    member x.Test2() = 
+        Assert.AreEqual(simpl (Sub(Var "x", Var "x")), Const 0, "error")
+    [<Test>]
+    member x.Test3() = 
+        Assert.AreEqual(simpl (Div(Mul(Add(Const 0, Const 2),Add(Const 3, Const 4)), Sub(Const 5, Const 3))), Const 7, "error")
+    [<Test>]
+    member x.Test4() = 
+        Assert.AreEqual(simpl (Mul(Add(Var "x", Const 4), Const 0)), Const 0 , "error")
+    [<Test>]
+    member x.Test5() = 
+        Assert.AreEqual(simpl (Sub(Mul(Const 13, Const 1), Mul(Const 5, Const 2))), Const 3, "error")
+    [<Test>]
+    member x.Test6() = 
+        Assert.AreEqual(simpl (Add(Var "x", Var "y")), Add(Var "x", Var "y"), "error")
+    [<Test>]
+    member x.Test7() = 
+        Assert.AreEqual(simpl (Add(Mul(Sub(Const 5, Const 2), Var "x"), Mul(Mul(Const 3, Const 2), Var "y"))), Add(Mul(Const 3, Var "x"), Mul(Const 6, Var "y")), "error")
+    [<Test>]
+    member x.Test8() = 
+        Assert.AreEqual(simpl (Sub(Const 4, Div(Const 8, Const 4))), Const 2, "error")
+    [<Test>]
+    member x.Test9() = 
+        Assert.AreEqual(simpl (Add(Div(Const 2, Mul(Const 1, Const 1)), Const 0)), Const 2, "error")
